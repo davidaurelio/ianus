8af743027506e82519d15e9aeb89ea9d5157acc9
David Aurelio <dev@david-aurelio.com>
2014-09-22 17:37:48 +0200

    Switch from saucelabs to browserstack

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -1,56 +1,61 @@
 var setValues = module.exports = require('./karma.conf');
 var configValues = require('./karma.conf').configValues;
 var env = process.env;
+var packageInfo = require('./package.json');
 
-// Use ENV vars for credentials
-if (!env.SAUCE_USERNAME || !env.SAUCE_ACCESS_KEY) {
-  throw Error('Provide sauce access credentials as SAUCE_USERNAME and SAUCE_ACCESS_KEY env variables.');
-}
-
-// Browsers to run on Sauce Labs
+// Browsers to run on BrowserStack
 var customLaunchers = {
-  'SL_IOS_5': createLauncher('iphone', '5.1'),
-  'SL_IOS_7': createLauncher('iphone', '7.1'),
-  'SL_Android_4.4': createLauncher('android', '4.4'),
-  'SL_Android_4.3': createLauncher('android', '4.3'),
-  'SL_Android_4.2': createLauncher('android', '4.2'),
-  'SL_Android_4.1': createLauncher('android', '4.1'),
-  'SL_Android_4.0': createLauncher('android', '4.0'),
-  'SL_IE_11': createLauncher('internet explorer', '11'),
-  'SL_IE_10': createLauncher('internet explorer', '10'),
-  'SL_IE_9': createLauncher('internet explorer', '9'),
-  'SL_IE_8': createLauncher('internet explorer', '8'),
-  'SL_FF_15': createLauncher('firefox', '15'),
-  'SL_FF_latest': createLauncher('firefox'),
-  'SL_Chrome_15': createLauncher('chrome', '15'),
-  'SL_Chrome_latest': createLauncher('chrome'),
-  'SL_Safari_5': createLauncher('safari', '5'),
-  'SL_Safari_latest': createLauncher('safari'),
-  'SL_Opera_12': createLauncher('opera', '12')
+  'BS_IOS_6': createDevice('ios', '6.1', 'iPhone 5'),
+  'BS_IOS_latest': createDevice('ios', '7.1', 'iPhone 5S'),
+  'BS_Android_2.3': createDevice('android', '2.3', 'Motorola Photon 4G'),
+  'BS_Android_4.0': createDevice('android', '4.0', 'HTC One X'),
+  'BS_Android_4.1': createDevice('android', '4.1', 'Motorola Razr Maxx HD'),
+  'BS_Android_4.2': createDevice('android', '4.2', 'LG Nexus 4'),
+  // 'BS_Android_latest': createDevice('android', 'latest', '???'),
+  'BS_IE_11': createBrowser('ie', '11.0'),
+  'BS_IE_10': createBrowser('ie', '10.0'),
+  'BS_IE_9': createBrowser('ie', '9.0'),
+  'BS_IE_8': createBrowser('ie', '8.0'),
+  'BS_FF_15': createBrowser('firefox', '15'),
+  'BS_FF_latest': createBrowser('firefox', 'latest'),
+  'BS_Chrome_25': createBrowser('chrome', '25'),
+  'BS_Chrome_latest': createBrowser('chrome', 'latest'),
+  'BS_Safari_5.1': createBrowser('safari', '5.1', 'OS X', 'Snow Leopard'),
+  'BS_Safari_6.1': createBrowser('safari', '6.1', 'OS X', 'Mountain Lion'),
+  'BS_Safari_latest': createBrowser('safari', 'latest', 'OS X', 'Mavericks'),
+  'BS_Opera_12': createBrowser('opera', '12.16')
 };
 
 var launcherNames = Object.keys(customLaunchers);
-if (env.SAUCE_RANGE) {
-  var range = env.SAUCE_RANGE.split(',').map(Number);
-  launcherNames = launcherNames.slice(range[0], range[0] + range[1]);
-}
 
-configValues.reporters = ['dots', 'saucelabs'];
-configValues.sauceLabs = {testName: 'ianus runtime environment tests'};
+configValues.reporters = ['dots'];
 configValues.customLaunchers = customLaunchers;
 configValues.browsers = launcherNames;
 configValues.singleRun = true;
-configValues.captureTimeout = 300000;
+configValues.browserDisconnectTolerance = 2;
+configValues.browserNoActivityTimeout = 60000;
+configValues.browserStack = {
+  project: packageInfo.name,
+  build: process.env.BROWSER_STACK_BUILD || packageInfo.version
+};
 
-function createLauncher(name, version, platform) {
-  var launcher = {
-    base: 'SauceLabs',
-    browserName: name
+function createDevice(os, version, device) {
+  return {
+    base: 'BrowserStack',
+    os: os,
+    os_version: version,
+    device: device
   };
-  if (version) {
-    launcher.version = version;
-  }
-  return launcher;
+}
+
+function createBrowser(name, version, os, osVersion) {
+  return {
+    base: 'BrowserStack',
+    browser: name,
+    browser_version: version,
+    os: os || 'Windows',
+    os_version: osVersion || 7
+  };
 }
 
 if (require.main === module) {
